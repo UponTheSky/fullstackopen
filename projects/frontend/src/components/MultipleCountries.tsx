3f56373b9ef928946f2e77a920272005b5d1804f
UponTheSky <sailing.to.orbis@gmail.com>
2022-10-22 23:59:02 +0900

    part2: ex.2.13

--- projects/frontend/src/components/MultipleCountries.tsx
+++ projects/frontend/src/components/MultipleCountries.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CountryType } from '../types';
+
+import { SingleCountry } from './SingleCountry';
 
 interface MultipleCountriesProps {
   countries: CountryType[];
 }
 
 export function MultipleCountries({ countries }: MultipleCountriesProps) {
+  const [isShowed, setIsShowed] = useState<{[key: string]: boolean }>(() => (
+    countries
+    .map(country => country.name.common)
+    .reduce((prev, curr) => ({ ...prev, [curr]: false }), {})
+  ));
+
+  const handleSetShowCountry = (name: string) => () => {
+    setIsShowed(prev => ({ ...prev, [name]: !prev[name]}))
+  };
+
   return (
     <div>
       {countries.length > 10 
       ? "Too many matches, specify another filter"
       : countries.map(country => (
-        `${country.name.common}\n`
+        <div>
+          {country.name.common} <button onClick={handleSetShowCountry(country.name.common)}>show</button>
+          {isShowed[country.name.common] ? <SingleCountry country={country} />: null}
+        </div>
       ))
       }
     </div>
